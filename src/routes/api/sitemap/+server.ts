d5520de8f75aa9bd6993942a8b88fc674762245b
Justin Schilleman <justin@schilleman.com>
2023-08-31 18:37:32 -0400

    testing 4

--- src/routes/api/sitemap/+server.ts
+++ src/routes/api/sitemap/+server.ts
@@ -1,11 +1,9 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { readdirSync } from 'fs';
+import dirTree from 'directory-tree';
 
 export const GET: RequestHandler = () => {
-	const directories = {
-		dot: readdirSync('./'),
-		back: readdirSync('../'),
-		sveltekit: readdirSync('./.svelte-kit/')
-	};
-	return json(directories);
+	const directory = dirTree('./.svelte-kit/');
+
+	return json(directory);
 };
