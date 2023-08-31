b4909772e714c001965554d12c10c2b3a8b39257
Justin Schilleman <justin@schilleman.com>
2023-08-31 18:42:14 -0400

    fake it till you make it

--- src/routes/api/sitemap/+server.ts
+++ src/routes/api/sitemap/+server.ts
@@ -1,9 +1,18 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
-import { readdirSync } from 'fs';
-import dirTree from 'directory-tree';
+import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = () => {
-	const directory = dirTree('./.svelte-kit/');
-
-	return json(directory);
+	return new Response(
+		`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://www.jschillem.dev/</loc>
+<lastmod>2023-08-31</lastmod>
+</url>
+</urlset>`,
+		{
+			headers: {
+				'Content-Type': 'text/xml'
+			}
+		}
+	);
 };
